da102e841ab7a67da08b208e4f2fb9f9b019829f
Satya Garapati <garapati@dotzilla.de>
2025-03-21 16:07:12 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using Microsoft.Data.SqlClient;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        string connectionString = "Server=devsql.devzilla.local;Database=fzgboerse_live;User Id=sa;Password=Dotzilla2204;Encrypt=False;TrustServerCertificate=True;";
+
+        using (var connection = new SqlConnection(connectionString))
+        {
+            try
+            {
+                connection.Open();
+                Console.WriteLine("Connection successful!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+    }
+}
